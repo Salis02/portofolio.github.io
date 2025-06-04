a1b2fc3126c20b03b6d33275f248a22cd127af26
Salis02 <nizarsalis@gmail.com>
2025-06-04 22:04:20 +0700

    modifikasi teks

--- script.js
+++ script.js
@@ -56,7 +56,7 @@ ScrollReveal().reveal('.home-content p, .about-content', {origin: 'right'});
 // =============== typed js ===================
 
 const typed = new Typed('.multiple-text',{
-    strings:['Frontend Developer','Mobile Developer','UI/UX Designer','Content Creator'],
+    strings:['Fullstack Developer','Content Creator'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
